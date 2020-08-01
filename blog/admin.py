30e70102f2c7540c441770a3ab3303c0d24eee9d
MohGumaa <gomaam14@gmail.com>
2020-08-01 10:30:23 +0530

    create home page and update admin page

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = "Blog Tweet Administration page"
+admin.site.site_title = "Blog Tweet  Admin Area"
+admin.site.index_title = "Welcome to Blog Tweet admin area"
---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Smith John',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2020'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {"title":"About"})
